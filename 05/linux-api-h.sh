352ef4fb9bf42ca2b07be98c48a36b1f1a23cb21
Dan Morris <dan@dmonline.uk>
2025-12-20 23:41:51 +0000

    linux api headers script

---
+++ 05/linux-api-h.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+here=`pwd`
+cd $LFS/sources
+tar xf linux-6.18.1.tar.xz
+cd linux-6.18.1
+
+make mrproper
+
+make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
+
+cd $here
+
+echo "If complete, delete 'rm -rf $LFS/sources/linux-6.18.1'"
