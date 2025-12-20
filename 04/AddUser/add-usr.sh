ec4c51a2f63ab0f5776e5ea515e1a10f3d9c0a91
Dan Morris <dan@dmonline.uk>
2025-12-20 22:41:53 +0000

    user profile creations scripts

--- 04/AddUser/add-usr.sh
+++ 04/AddUser/add-usr.sh
@@ -9,5 +9,7 @@ case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
 
+[ ! -e /etc/bash.bashrc ] || mv -v /etc/bash.bashrc /etc/bash.bashrc.NOUSE
+
 echo "Please switch to the lfs user:"
 echo "'su - lfs'"
